2fc0a8c8a3bcbb777f92dca35510c4cfe0028d8d
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2024-04-09 12:38:10 +0200

    Summary initial

---
+++ src/main.js
@@ -0,0 +1,16 @@
+`use strict`
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// iziToast.show({
+//     iconUrl: icon,
+//     message:"Please choose a date in the future",
+//     messageColor: '#ffffff',
+//     color: '#ef4040',
+//     close: true,
+//     position: 'topRight',
+   
+//   });
