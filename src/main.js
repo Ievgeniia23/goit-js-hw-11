f805e980964b5c783ecff76b5587dd3e736cc92c
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2024-04-10 15:23:04 +0200

    API request

--- src/main.js
+++ src/main.js
@@ -5,6 +5,38 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { fetchData } from "./js/pixabay-api";
+
+
+const btnSubmit = document.querySelector("button");
+const form = document.querySelector("form");
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputValue = document.querySelector("input").value;
+    
+
+    fetchData(inputValue)
+    .then(data => console.log(data))
+    .catch(error => console.log("catch", error))
+
+});
+
+ 
+    
+
+    
+
+
+
+   
+
+
+
+
+
 // iziToast.show({
 //     iconUrl: icon,
 //     message:"Please choose a date in the future",
