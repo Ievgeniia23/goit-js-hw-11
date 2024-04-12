269fe4e3620f218ecc521b0d49136ad9830ec1bb
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2024-04-13 00:22:55 +0200

    Summary

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,6 +4,7 @@ export function createMarkup(arr) {
 return arr.map(({webformatURL, largeImageURL,
      tags, likes, views, comments, downloads 
      }) => ` 
+    
      <li class="gallery-item">
 <a class="gallery-link" href="${largeImageURL}">
   <img
@@ -11,18 +12,32 @@ return arr.map(({webformatURL, largeImageURL,
     src="${webformatURL}"
     data-source="${largeImageURL}"
     alt="${tags}"
-    data-likes="${likes}"
-    data-views="${views}"
-    data-comments="${comments}"
-    data-downloads="${downloads}"
- />
-</a>
-</li>`).join('')
+   >
+  </a>
 
-}
+  <ul class="text-card">
+    <li class="text-card-item">
+      <h3>Likes</h3>
+      <p>${likes}</p>
+    </li>
 
+    <li class="text-card-item">
+      <h3>Views</h3>
+      <p>${views}</p>
+    </li>
 
-   
+    <li class="text-card-item">
+      <h3>Comments</h3>
+      <p>${comments}</p>
+    </li>
+
+    <li class="text-card-item">
+      <h3>Downloads</h3>
+      <p>${downloads}</p>
+    </li>
+</ul>
  
+</li>`).join('');
+}
 
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import icon from "./img/Group.svg"
 
 import { fetchData } from "./js/pixabay-api";
 
@@ -12,28 +13,55 @@ import { createMarkup } from "./js/render-functions";
 
 const form = document.querySelector("form");
 const gallery = document.querySelector("ul")
+const loader = document.querySelector(".loader")
 
-
+loader.style.display = "none"
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const inputValue = document.querySelector("input").value;
-    
+
+    gallery.innerHTML = "";
+
+    loader.style.display = "block"
 
     fetchData(inputValue)
-    .then(data => gallery.insertAdjacentHTML("beforeend",createMarkup(data.hits)))
-    .catch(error => console.log("catch", error))
+        .then(data => {
+            if(data.hits.length === 0) {
+                iziToast.show({
+                    iconUrl: icon,
+                    message:"Sorry, there are no images matching your search query. Please try again!",
+                    messageColor: '#ffffff',
+                    color: '#ef4040',
+                    close: true,
+                    position: 'topRight',
+                             
+                                });
+              } else { gallery.insertAdjacentHTML("beforeend",createMarkup(data.hits))
+              
+                    const lightbox = new SimpleLightbox('.gallery a', { 
+     
+                        captionsData: `alt`,
+                        captionDelay: 250,
+                        });
+                
+                    lightbox.refresh();
+                    }
 
+                
+            })
+    
+        .catch(error => console.log("catch", error)) 
+        .finally(() => {
+            
+            loader.style.display = "none";
+        });
+   
+    form.reset();
 });
 
 
-const lightbox = new SimpleLightbox('.gallery a', { 
-     
-    captionsData: `alt`,
-    captionDelay: 250,
-      
-    });
-    
+
 
     
 
@@ -45,12 +73,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
 
 
-// iziToast.show({
-//     iconUrl: icon,
-//     message:"Please choose a date in the future",
-//     messageColor: '#ffffff',
-//     color: '#ef4040',
-//     close: true,
-//     position: 'topRight',
-   
-//   });
